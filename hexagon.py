a4558e414a2a9b3c0962789b645309b4efe647f3
Sumit Uttam <57025866+SumitUttam@users.noreply.github.com>
2019-12-09 15:30:09 +0530

    added randomness to the colours and size

--- hexagon.py
+++ hexagon.py
@@ -1,25 +1,30 @@
 import turtle
-from random import randint
-  
+from random import radiant,choice
+
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
 hexagon = turtle.Turtle() 
 hexagon.speed(1)
 hexagon.right(randint(0,360))
 x = randint(30,100)
 
-for i in range(6): 
+for i in range(6):
+    hexagon.color(choice(colors))
     hexagon.forward(x) 
     hexagon.right(60) 
 
 hexagon.right(30)
 for i in range(3):
+    hexagon.color(choice(colors))
     hexagon.forward(x*(3**0.5)) 
     hexagon.right(120)
 
 hexagon.left(30)
+hexagon.color(choice(colors))
 hexagon.forward(x)
 hexagon.right(90) 
 
 for i in range(3):
+    hexagon.color(choice(colors))
     hexagon.forward(x*(3**0.5)) 
     hexagon.right(120)
       
