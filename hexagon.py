ef5ee0fedc4f2d09d2d13df11d23743cade2a90b
Sumit Uttam <sumitu0812@gmail.com>
2019-12-12 14:02:02 +0530

    fixed a small bug

--- hexagon.py
+++ hexagon.py
@@ -2,31 +2,35 @@ import turtle
 from random import randint,choice
 
 colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
-hexagon = turtle.Turtle() 
-hexagon.speed(1)
-hexagon.right(randint(0,360))
-x = randint(30,100)
-hexagon.width(x/10-3)
+def Hexa (x,y):
+    hexagon = turtle.Turtle() 
+    hexagon.speed(1)
+    hexagon.right(randint(0,360))
+    x = randint(30,100)
+    hexagon.width(x/10-3)
 
-for i in range(6):
+    for i in range(6):
+        hexagon.color(choice(colors))
+        hexagon.forward(x) 
+        hexagon.right(60) 
+
+    hexagon.right(30)
+    for i in range(3):
+        hexagon.color(choice(colors))
+        hexagon.forward(x*(3**0.5)) 
+        hexagon.right(120)
+
+    hexagon.left(30)
     hexagon.color(choice(colors))
-    hexagon.forward(x) 
-    hexagon.right(60) 
+    hexagon.forward(x)
+    hexagon.right(90) 
 
-hexagon.right(30)
-for i in range(3):
-    hexagon.color(choice(colors))
-    hexagon.forward(x*(3**0.5)) 
-    hexagon.right(120)
+    for i in range(3):
+        hexagon.color(choice(colors))
+        hexagon.forward(x*(3**0.5)) 
+        hexagon.right(120)
+        
+    turtle.done() 
 
-hexagon.left(30)
-hexagon.color(choice(colors))
-hexagon.forward(x)
-hexagon.right(90) 
-
-for i in range(3):
-    hexagon.color(choice(colors))
-    hexagon.forward(x*(3**0.5)) 
-    hexagon.right(120)
-      
-turtle.done() 
+if __name__ == "__main__":
+    Hexa(0,0);
