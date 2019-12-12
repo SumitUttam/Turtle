bf3ab6e0a20b74febd309d43b5083659211866b9
Sumit Uttam <sumitu0812@gmail.com>
2019-12-12 14:21:18 +0530

    minor changes for readability

--- RainbowBenzene.py
+++ RainbowBenzene.py
@@ -1,14 +1,18 @@
 import turtle 
-colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow'] 
-turtle.setup(1280,720)
-RainbowBenzene = turtle.Pen() 
-turtle.bgcolor('black') 
-RainbowBenzene.shape('turtle')
-RainbowBenzene.speed(0)
-for x in range(360): 
-    RainbowBenzene.pencolor(colors[x%6]) 
-    RainbowBenzene.width(x/100 + 1) 
-    RainbowBenzene.forward(x) 
-    RainbowBenzene.left(59)
 
-input() 
+def RBenzene ():
+    colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow'] 
+    turtle.setup(1280,720)
+    RainbowBenzene = turtle.Pen() 
+    turtle.bgcolor('black') 
+    RainbowBenzene.shape('turtle')
+    RainbowBenzene.speed(0)
+    for x in range(360): 
+        RainbowBenzene.pencolor(colors[x%6]) 
+        RainbowBenzene.width(x/100 + 1) 
+        RainbowBenzene.forward(x) 
+        RainbowBenzene.left(59)
+    turtle.done()
+    
+if __name__ == "__main__":
+    RBenzene()
